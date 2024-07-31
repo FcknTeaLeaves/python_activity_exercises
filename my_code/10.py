dad0414c18d33175ebca8328a5c23f0b931bfaea
TeaLeaves <94555070+FcknTeaLeaves@users.noreply.github.com>
2024-07-31 11:51:10 +0000

    exercise 10

---
+++ my_code/10.py
@@ -0,0 +1,23 @@
+import random
+p = True
+while p = True
+sol = randint(1;100)
+
+w = 0
+
+while w =! 1:
+
+    guess = input("guessa number") 
+
+    elif guess > sol:
+        print("too high")
+    elif guess < sol:
+        print("too low")
+    elif guess = sol:
+        print("corretc!")
+        w = 1
+
+if input("restart? (y/n)") == y or Y:
+    p = True
+else 
+    p = False
